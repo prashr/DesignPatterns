cac8a1ae68bed8779d2ac4b2337e536efa483ea3
prasraje <prashanth.rajendra@nokia.com>
2019-01-17 10:15:49 +0530

    First Proj for 2
    Change-Id: I7e8848dce59617dd170580e45f0749944a486514

--- src/com/pra/dp/sing/Singleton.java
+++ src/com/pra/dp/sing/Singleton.java
@@ -1,5 +1,23 @@
 package com.pra.dp.sing;
 
 public class Singleton {
+	
+
+	    private static volatile Singleton instance;
+		private int i = 1;
+	    
+	    private Singleton(){
+	    	System.out.println("The Instance is created " + i++  );
+	    }
+	    
+	    public static Singleton getInstance(){
+	        if(instance == null){
+	        	synchronized(Singleton.class) {
+	        		instance = new Singleton();
+	        	}
+	        }
+	        return instance;
+	    }
+	    
 
 }
---
+++ src/com/pra/dp/sing/SingletonClient.java
@@ -0,0 +1,9 @@
+package com.pra.dp.sing;
+
+public class SingletonClient {
+
+	public static void main(String[] args) {
+
+	}
+
+}
